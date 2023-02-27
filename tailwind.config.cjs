f5ad12f026455fe9785d87749ed32640b7eea0ff
Facundo <facundojsole@gmail.com>
2023-02-27 13:35:01 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "'Work Sans', sans-serif",
+      },
+        
+    },
+  },
+  plugins: [],
+}
